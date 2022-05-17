d84ca0ac991ca1eee32fef196ac21a875b6d6089
Brice <briceklog@gmail.com>
2022-05-18 00:39:03 +0100

    project description

--- src/containers/welcome/Welcome.tsx
+++ src/containers/welcome/Welcome.tsx
@@ -13,6 +13,7 @@ function Welcome() {
       <div className='welcome__container-text'>
         <p>&ndash; Your First quote offered.</p>
         <h1>Don't waste any more time. The time has come to realize your project.</h1>
+        <p>Would you like a quote for an IT service or a personalized web/mobile application? we will give you a quote.</p>
       </div>
       <div className='welcome__container-quote'>
         <button>ASK FOR QUOTE</button>
