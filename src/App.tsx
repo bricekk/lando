a3aabaa2c69cf2184e1ab97bf795ea38009fdd20
Brice <briceklog@gmail.com>
2022-05-17 23:27:21 +0100

    welcome container done!

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import ThemeProvider from './utils/themeProvider/ThemePovider';
 
 import { Navbar } from './components';
+import { Welcome } from './containers';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar/>
+      <Welcome/>
     </ThemeProvider>
   );
 }
